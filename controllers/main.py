b59ed9832bb1a8cb5313039003854569b34cd791
vagrant <vagrant@vagrant.vm>
2016-09-09 04:25:41 +0000

    test1

---
+++ controllers/main.py
@@ -0,0 +1,12 @@
+from flask import *
+
+main = Blueprint('main', __name__, template_folder='templates')
+
+@main.route('/')
+def main_route():
+    return render_template("index.html")
+
+@main.route('/hello')
+def hello():
+      return "Hello World! Hello Everyone!"
+
