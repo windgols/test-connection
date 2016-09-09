a658a95e7e0739ff53fd10a4feefdfcfbde6f261
vagrant <vagrant@vagrant.vm>
2016-09-09 04:51:53 +0000

    the right way to do things

--- controllers/main.py
+++ controllers/main.py
@@ -2,9 +2,9 @@ from flask import *
 
 main = Blueprint('main', __name__, template_folder='templates')
 
-#@main.route('/')
-#def main_route():
-#    return render_template("index.html")
+@main.route('/')
+def main_route():
+    return render_template("index.html")
 
 #@main.route('/hello')
 #def hello():
